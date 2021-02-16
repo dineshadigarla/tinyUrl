32ed50b334783282ba55cccf74eb144c8b4ab43d
Dinesh Adigarla <DineshA@talentica-all.com>
2021-02-17 03:18:31 +0530

    Sample TinyUrl Application

---
+++ src/main/java/com/tinyurl/repository/UrlRepository.java
@@ -0,0 +1,9 @@
+package com.tinyurl.repository;
+
+import com.tinyurl.entity.Url;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UrlRepository extends JpaRepository<Url, Long> {
+}
